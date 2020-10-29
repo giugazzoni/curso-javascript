d8be582e6b70d7fd4aefbc8d41789d4e52f45202
giugazzoni <giugazz@gmail.com>
2020-10-29 19:35:44 -0300

    aula 16 conceitos e prática

---
+++ aula16/funcao05.js
@@ -0,0 +1,10 @@
+// Recursividade (uma finção está chamando ela mesma)
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n - 1)
+    }
+}
+
+console.log(fatorial(5))
